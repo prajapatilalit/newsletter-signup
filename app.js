380513311071384a041921212d6c2a7e20f73161
prajapatilalit <prajapatilalit150992@gmail.com>
2021-07-29 11:15:06 +0530

    update static public folder in app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const request = require("request");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname, "/signup.html");
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.listen(port, function () {
